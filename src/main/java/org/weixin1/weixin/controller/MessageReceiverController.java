d71b7e3a0509ab0c15df1a9d3ec65a225df5079b
Administrator <Administrator@7ZEJ5TOENQ9LHUB>
2019-03-31 16:42:42 +0800

    完成简单的微信接入程序开发

---
+++ src/main/java/org/weixin1/weixin/controller/MessageReceiverController.java
@@ -0,0 +1,21 @@
+package org.weixin1.weixin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/csn/message/receiver")
+public class MessageReceiverController {
+
+@GetMapping
+public String echo{//
+	@RequestParam("signature") String signature,//
+	@RequestParam("timestamp") String timestamp,//
+	@RequestParam("nonce") String nonce,//
+	@RequestParam("echostr") String echostr//
+	//
+}{
+	return echostr;
+	
+}
+}
